254ad87958b3bf0f518cc8ac16bbc06a200f58b4
BenjaminGonzalezH <benjamin.gonzalez.h@usach.cl>
2024-10-11 16:24:22 -0300

    first function

--- App/CoMOcG/src/ReadSolution.py
+++ App/CoMOcG/src/ReadSolution.py
@@ -1,9 +1,37 @@
 ######### Libraries #########
 import numpy as np
-import pandas as pd
+import csv
 
-######### Libraries #########
+######### Error Codes #########
 
-def ReadDataFrame(filename):
-    df = pd.read_csv(filename)
-    return df
+
+######### Functions #########
+
+def ReadInputcsv(filepath):
+    """
+        ReadInput (function)
+            - filepath: filepath of your Multiobjective
+              gene clustering results.
+    """
+    try:
+        # Gene clustering results matrix.
+        Matrix = []
+
+        # Open CSV file.
+        with open(filepath, mode = "r") as file:
+            # Start reading process.
+            Csvfile = csv.reader(file)
+
+            # List of genes.
+            genes = next(Csvfile)
+            n = len(genes)
+
+            # Read every line.
+            for line in Csvfile:        
+                # Take just int values.
+                Matrix.append(line[1:n+1])
+
+        return np.array(Matrix, dtype=int), genes, n
+    
+    except FileNotFoundError:
+        return "FNF"
