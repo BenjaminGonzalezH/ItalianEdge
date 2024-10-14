2d63de8129882ab189b765f331b146c2b5e1b25b
BenjaminGonzalezH <benjamin.gonzalez.h@usach.cl>
2024-10-14 14:50:50 -0300

    Reading functions (I/O test pending)

--- App/CoMOcG/src/ReadSolution.py
+++ App/CoMOcG/src/ReadSolution.py
@@ -1,22 +1,42 @@
 ######### Libraries #########
+import os
 import numpy as np
 import csv
-
-######### Error Codes #########
-
 
 ######### Functions #########
 
 def ReadInputcsv(filepath):
     """
-        ReadInput (function)
+    ReadInput (function)
+        Input:
             - filepath: filepath of your Multiobjective
               gene clustering results.
+        Output:
+            - Matrix: Integer values matrix thar allocates
+            numbers of clusters associated with every gene.
+            - gene: List of the genes used in the study.
+            - n: amount of genes.
+
+        Description: This function reads a .csv file with the
+        following format.
+                    Gene 1 | Gene 2 | Gene 3 | ... | Gene n
+        Solution 1|    1        3       4               2
+        Solution 2|    3        4       1               2
+        Solution 3|
+        ...
+        Solution k|
+
+        Every cell in the "dataframe" allocates the number
+        of the cluster where the gene is in the actual solution.
     """
     try:
         # Gene clustering results matrix.
         Matrix = []
 
+        # Empty file verification.
+        if os.path.getsize(filepath) == 0:
+            raise ValueError("Empty file.")
+        
         # Open CSV file.
         with open(filepath, mode = "r") as file:
             # Start reading process.
@@ -34,8 +54,56 @@ def ReadInputcsv(filepath):
         return np.array(Matrix, dtype=int), genes, n
     
     except FileNotFoundError:
-        return "FNF"
+        raise ValueError(f"File in {filepath} does not exists.")
     except OSError:
-        return "I/O_E"
-    except EOFError:
-        return "EOF_E"
+        raise ValueError("Can not read the file due to a I/O error.")
+    
+def ReadInputcsv_noID(filepath):
+    """
+    ReadInput (function)
+        Input:
+            - filepath: filepath of your Multiobjective
+              gene clustering results.
+        Output:
+            - Matrix: Integer values matrix thar allocates
+            numbers of clusters associated with every gene.
+            - gene: List of the genes used in the study.
+            - n: amount of genes.
+
+        Description: This function reads a .csv file with the
+        following format.
+        Gene 1 | Gene 2 | Gene 3 | ... | Gene n
+            1        3       4               2
+            3        4       1               2  
+
+        Every cell in the "dataframe" allocates the number
+        of the cluster where the gene is in the actual solution.
+    """
+    try:
+        # Gene clustering results matrix.
+        Matrix = []
+
+        # Empty file verification.
+        if os.path.getsize(filepath) == 0:
+            raise ValueError("Empty file.")
+        
+        # Open CSV file.
+        with open(filepath, mode = "r") as file:
+            # Start reading process.
+            Csvfile = csv.reader(file)
+
+            # List of genes.
+            genes = next(Csvfile)
+            n = len(genes)
+
+            # Read every line.
+            for line in Csvfile:        
+                # Take just int values.
+                Matrix.append(line[0:n+1])
+
+        return np.array(Matrix, dtype=int), genes, n
+    
+    except FileNotFoundError:
+        raise ValueError(f"File in {filepath} does not exists.")
+    except OSError:
+        raise ValueError("Can not read the file due to a I/O error.")
--- App/CoMOcG/test/ReadSolution_Tests.py
+++ App/CoMOcG/test/ReadSolution_Tests.py
@@ -12,18 +12,23 @@ import numpy.testing as npt
 ######### Module Path #########
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
 from ReadSolution import (
-    ReadInputcsv
+    ReadInputcsv,
+    ReadInputcsv_noID
 )
 
 ######### Aux functions #########
 def dumb_alphaNumeric():
-    letras = ''.join(random.choices(string.ascii_uppercase, k=6))  # 6 letras aleatorias
-    numeros = ''.join(random.choices(string.digits, k=4))  # 4 dígitos aleatorios
+    letras = ''.join(random.choices(string.ascii_uppercase, k=6)) 
+    numeros = ''.join(random.choices(string.digits, k=4))
     return letras + numeros
 
 def generate_solution(numero, tam):
     solucion = [f"Solution {numero}"]
-    solucion.extend(random.randint(1, 4) for _ in range(tam))  # 3 números aleatorios entre 1 y 10
+    solucion.extend(random.randint(1, 4) for _ in range(tam))
+    return solucion
+
+def generate_solution_noID(numero, tam):
+    solucion = [random.randint(1, 4) for _ in range(tam)]
     return solucion
 
 ######### Testing #########
@@ -57,12 +62,126 @@ class TestReadSolution(unittest.TestCase):
             self.assertEqual(num_genes, 20)
             npt.assert_array_equal(Matrix, compareMatrix)
 
-    def test_read_filenotfound(self):
-        # Function trying to read a non-existing file.
-        Output = ReadInputcsv("NoFile")
-        # Recieve error code.
-        self.assertCountEqual(Output,"FNF")
-        
+    def test_read_filenotfound(self):       
+        # Obtain ValueError from function.
+        with self.assertRaises(ValueError) as context:
+            ReadInputcsv("NoFile")
+        self.assertEqual(str(context.exception), "File in NoFile does not exists.")
+
+    def test_read_Emptyfile(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            csv.writer(temp_csv)
+            # close tempfile to re-open it later.
+            temp_csv.close()
+
+            # Obtain ValueError from function.
+            with self.assertRaises(ValueError) as context:
+                ReadInputcsv(temp_csv.name)
+            self.assertEqual(str(context.exception), "Empty file.")
+
+            os.remove(temp_csv.name)
+
+    def test_read_Emptyrow(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            
+            # Write file.
+            for _ in range(10):
+                writer.writerow([])
+            empty_matrix = np.empty((9, 0), dtype=np.int64)
+            
+            # close tempfile to re-open it later.
+            temp_csv.close()
+
+            # function.
+            Matrix, genes, num_genes = ReadInputcsv(temp_csv.name)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, [])
+            self.assertEqual(num_genes, 0)
+            self.assertTrue(np.array_equal(Matrix, empty_matrix))
+
+class TestReadSolutionNoID(unittest.TestCase):
+    def test_read_valid_file(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            compareMatrix = []
+            # Write file.
+            dumb_genes = [dumb_alphaNumeric() for _ in range(20)]
+            writer.writerow(dumb_genes)
+            for i in range(0,101):
+                row = generate_solution_noID(i,20)
+                writer.writerow(row)
+                compareMatrix.append(row[0:len(row)+1])
+            compareMatrix = np.array(compareMatrix, dtype=int)
+
+            # close tempfile to re-open it later.
+            temp_csv.close()
+            
+            # function.
+            Matrix, genes, num_genes = ReadInputcsv_noID(temp_csv.name)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, dumb_genes)
+            self.assertEqual(num_genes, 20)
+            npt.assert_array_equal(Matrix, compareMatrix)
+
+    def test_read_filenotfound(self):       
+        # Obtain ValueError from function.
+        with self.assertRaises(ValueError) as context:
+            ReadInputcsv_noID("NoFile")
+        self.assertEqual(str(context.exception), "File in NoFile does not exists.")
+
+    def test_read_Emptyfile(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            csv.writer(temp_csv)
+            # close tempfile to re-open it later.
+            temp_csv.close()
+
+            # Obtain ValueError from function.
+            with self.assertRaises(ValueError) as context:
+                ReadInputcsv_noID(temp_csv.name)
+            self.assertEqual(str(context.exception), "Empty file.")
+
+            os.remove(temp_csv.name)
+
+    def test_read_Emptyrow(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            
+            # Write file.
+            for _ in range(10):
+                writer.writerow([])
+            empty_matrix = np.empty((9, 0), dtype=np.int64)
+            
+            # close tempfile to re-open it later.
+            temp_csv.close()
+
+            # function.
+            Matrix, genes, num_genes = ReadInputcsv_noID(temp_csv.name)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, [])
+            self.assertEqual(num_genes, 0)
+            self.assertTrue(np.array_equal(Matrix, empty_matrix))
+
 
 if __name__ == '__main__':
     unittest.main()
