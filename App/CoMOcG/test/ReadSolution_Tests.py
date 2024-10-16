4bc976842f65ed8bee4d73b69338b9028a9ff05b
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2024-10-16 00:38:12 -0300

    End of implementation (Reading CSV)

--- App/CoMOcG/test/ReadSolution_Tests.py
+++ App/CoMOcG/test/ReadSolution_Tests.py
@@ -52,7 +52,7 @@ class TestReadSolutionMT(unittest.TestCase):
             temp_csv.close()
             
             # function.
-            genes, num_genes, Matrix = ReadInputCSV(temp_csv.name,chunk_size=100, n_threads=6)
+            genes, num_genes, Matrix = ReadInputCSV(temp_csv.name, n_threads=5)
 
             # Delete the file. Also, NamedTemporaryFile do it, but
             # this happend when you close the file.
@@ -61,6 +61,93 @@ class TestReadSolutionMT(unittest.TestCase):
             # AssertEquals
             self.assertEqual(genes, dumb_genes)
             self.assertEqual(num_genes, 1000)
+            npt.assert_array_equal(Matrix, compareMatrix)
+
+    def test_read_valid_file_lessthreads(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            compareMatrix = []
+            # Write file.
+            dumb_genes = [dumb_alphaNumeric() for _ in range(100)]
+            writer.writerow(dumb_genes)
+            for i in range(0,50):
+                row = generate_solution(i,100)
+                writer.writerow(row)
+                compareMatrix.append(row[1:len(row)+1])
+            compareMatrix = np.array(compareMatrix, dtype=int)
+
+            # close tempfile to re-open it later.
+            temp_csv.close()
+            
+            # function.
+            genes, num_genes, Matrix = ReadInputCSV(temp_csv.name, n_threads=3)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, dumb_genes)
+            self.assertEqual(num_genes, 100)
+            npt.assert_array_equal(Matrix, compareMatrix)
+
+    def test_read_valid_file_eventhreads_notevenrows(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            compareMatrix = []
+            # Write file.
+            dumb_genes = [dumb_alphaNumeric() for _ in range(102)]
+            writer.writerow(dumb_genes)
+            for i in range(0,51):
+                row = generate_solution(i,102)
+                writer.writerow(row)
+                compareMatrix.append(row[1:len(row)+1])
+            compareMatrix = np.array(compareMatrix, dtype=int)
+
+            # close tempfile to re-open it later.
+            temp_csv.close()
+            
+            # function.
+            genes, num_genes, Matrix = ReadInputCSV(temp_csv.name, n_threads=2)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, dumb_genes)
+            self.assertEqual(num_genes, 102)
+            npt.assert_array_equal(Matrix, compareMatrix)
+
+    def test_read_valid_file_threadsOverflow(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            compareMatrix = []
+            # Write file.
+            dumb_genes = [dumb_alphaNumeric() for _ in range(102)]
+            writer.writerow(dumb_genes)
+            for i in range(0,51):
+                row = generate_solution(i,102)
+                writer.writerow(row)
+                compareMatrix.append(row[1:len(row)+1])
+            compareMatrix = np.array(compareMatrix, dtype=int)
+
+            # close tempfile to re-open it later.
+            temp_csv.close()
+            
+            # function.
+            genes, num_genes, Matrix = ReadInputCSV(temp_csv.name, n_threads=24)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, dumb_genes)
+            self.assertEqual(num_genes, 102)
             npt.assert_array_equal(Matrix, compareMatrix)
 
     def test_read_filenotfound(self):       
@@ -127,7 +214,7 @@ class TestReadSolution_NoIDMT(unittest.TestCase):
             temp_csv.close()
             
             # function.
-            genes, num_genes, Matrix = ReadInputCSV_NoID(temp_csv.name,chunk_size=100, n_threads=6)
+            genes, num_genes, Matrix = ReadInputCSV_NoID(temp_csv.name, n_threads=6)
 
             # Delete the file. Also, NamedTemporaryFile do it, but
             # this happend when you close the file.
@@ -136,6 +223,93 @@ class TestReadSolution_NoIDMT(unittest.TestCase):
             # AssertEquals
             self.assertEqual(genes, dumb_genes)
             self.assertEqual(num_genes, 1000)
+            npt.assert_array_equal(Matrix, compareMatrix)
+
+    def test_read_valid_file_lessthreads(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            compareMatrix = []
+            # Write file.
+            dumb_genes = [dumb_alphaNumeric() for _ in range(100)]
+            writer.writerow(dumb_genes)
+            for i in range(0,50):
+                row = generate_solution_noID(100)
+                writer.writerow(row)
+                compareMatrix.append(row[0:len(row)+1])
+            compareMatrix = np.array(compareMatrix, dtype=int)
+
+            # close tempfile to re-open it later.
+            temp_csv.close()
+            
+            # function.
+            genes, num_genes, Matrix = ReadInputCSV_NoID(temp_csv.name, n_threads=3)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, dumb_genes)
+            self.assertEqual(num_genes, 100)
+            npt.assert_array_equal(Matrix, compareMatrix)
+
+    def test_read_valid_file_eventhreads_notevenrows(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            compareMatrix = []
+            # Write file.
+            dumb_genes = [dumb_alphaNumeric() for _ in range(102)]
+            writer.writerow(dumb_genes)
+            for i in range(0,51):
+                row = generate_solution_noID(102)
+                writer.writerow(row)
+                compareMatrix.append(row[0:len(row)+1])
+            compareMatrix = np.array(compareMatrix, dtype=int)
+
+            # close tempfile to re-open it later.
+            temp_csv.close()
+            
+            # function.
+            genes, num_genes, Matrix = ReadInputCSV_NoID(temp_csv.name, n_threads=3)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, dumb_genes)
+            self.assertEqual(num_genes, 102)
+            npt.assert_array_equal(Matrix, compareMatrix)
+
+    def test_read_valid_file_threadsOverflow(self):
+        # Create a csv tempfile for this test with identificator temp_csv.
+        with tempfile.NamedTemporaryFile(mode="w+", delete=False, newline='') as temp_csv:
+            writer = csv.writer(temp_csv)
+            compareMatrix = []
+            # Write file.
+            dumb_genes = [dumb_alphaNumeric() for _ in range(102)]
+            writer.writerow(dumb_genes)
+            for i in range(0,51):
+                row = generate_solution_noID(102)
+                writer.writerow(row)
+                compareMatrix.append(row[0:len(row)+1])
+            compareMatrix = np.array(compareMatrix, dtype=int)
+
+            # close tempfile to re-open it later.
+            temp_csv.close()
+            
+            # function.
+            genes, num_genes, Matrix = ReadInputCSV_NoID(temp_csv.name, n_threads=24)
+
+            # Delete the file. Also, NamedTemporaryFile do it, but
+            # this happend when you close the file.
+            os.remove(temp_csv.name)
+
+            # AssertEquals
+            self.assertEqual(genes, dumb_genes)
+            self.assertEqual(num_genes, 102)
             npt.assert_array_equal(Matrix, compareMatrix)
 
     def test_read_filenotfound(self):       
