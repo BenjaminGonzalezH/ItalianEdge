0c11c9c50c006e82a7e99809749a205e0f32994f
Benjamin Gonzalez <benjamin.gonzalez.h@usach.cl>
2024-11-03 16:15:51 -0300

    End of He and testing

--- App/CoMOcG/test/He_Clustering_Test.py
+++ App/CoMOcG/test/He_Clustering_Test.py
@@ -3,6 +3,8 @@ import unittest
 import sys
 import os
 import numpy as np
+import io
+from contextlib import redirect_stdout
 
 ######### Module Path #########
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
@@ -15,36 +17,37 @@ class TestHeClustering(unittest.TestCase):
     def test_clustering_output_length(self):
         # Matriz de distancia de ejemplo
         ProportionMatrix = np.array([
-            [0.0, 1.0, 1.5, 0.5],
-            [1.0, 0.0, 1.2, 1.8],
-            [1.5, 1.2, 0.0, 2.5],
-            [0.5, 1.8, 2.5, 0.0]
+            [0.0, 1.0, 0.5, 0.5],
+            [1.0, 0.0, 0.2, 0.8],
+            [0.5, 0.2, 0.0, 0.5],
+            [0.5, 0.8, 0.5, 0.0]
         ])
         num_groups = 4
+        expected_result = [2, 1, 1, 2]
 
         # Llamada a la función
-        y_hc = He_clustering(ProportionMatrix, num_groups)
+        cluster = He_clustering(ProportionMatrix, num_groups, "dendograma.png", 0)
+        self.assertEqual(cluster,expected_result)
 
-        print(y_hc)
 
-        # Verificar que la salida no sea None
-        self.assertIsNotNone(y_hc, "La función devolvió None en lugar de una matriz de etiquetas")
-
-        # Verificar que la longitud de la salida es correcta
-        self.assertEqual(len(y_hc), ProportionMatrix.shape[0], "La longitud de las etiquetas no coincide con el número de muestras")
-
-    """def test_invalid_input(self):
+    def test_invalid_input(self):
         # ProportionMatrix no cuadrada para probar la excepción
         ProportionMatrix = np.array([
             [0.0, 1.0, 1.5],
             [1.0, 0.0, 1.2]
         ])
+        num_groups = 4
 
-        # Llamada a la función con entrada inválida
-        y_hc = He_clustering(ProportionMatrix, num_groups=2)
+        # Not using the print message.
+        f = io.StringIO()
+        with redirect_stdout(f):
+            #RUN.
+            cluster = He_clustering(ProportionMatrix, num_groups, "dendograma.png", 0)
+        # Take message.
+        mensaje_impreso = f.getvalue()
 
         # Verificar que la salida sea None cuando la entrada es incorrecta
-        self.assertIsNone(y_hc, "La función no manejó correctamente la matriz no cuadrada")"""
+        self.assertIsNone(cluster, "La función no manejó correctamente la matriz no cuadrada")
 
 if __name__ == "__main__":
     unittest.main()
