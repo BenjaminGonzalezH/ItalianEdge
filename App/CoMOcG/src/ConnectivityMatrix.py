5e2d17402ccb4a47e0e292ac2d2ac616a46a650d
BenjaminGonzalezH <benjamin.gonzalez.h@usach.cl>
2025-01-04 16:37:55 -0400

    CM fixed function and documentation

--- App/CoMOcG/src/ConnectivityMatrix.py
+++ App/CoMOcG/src/ConnectivityMatrix.py
@@ -3,8 +3,8 @@ import numpy as np                                  # Efficient Math Operations.
 import os                                           # OS callings.
 import itertools                                    # Efficient iterations.
 from scipy.sparse import csr_matrix                 # Compresed space row matrix.
-from concurrent.futures import ProcessPoolExecutor  # Process Administration.
 from concurrent.futures import ThreadPoolExecutor   # Threads Administration.
+from scipy.sparse import dok_matrix                 # Structure for efficient csr construction.
 
 ######### AUX Functions #########
 
@@ -13,21 +13,22 @@ This block contains all functions that are used repeatedly in other functions.
 In addition, their are used by threads for process concurrency.
 """
 
-def compute_connectivity(Solution: list[int]) -> csr_matrix:
+def compute_connectivity(Solution: np.ndarray) -> csr_matrix:
     """
-    compute_connectivity(function): 
-    Description: Procedure where the process are going to
+    compute_connectivity(function): Procedure where the process are going to
     check de solution and creates a connectivity matrix.
     
     Parameters:
-    Solution (list)
-        Output:
-            - connectivity Matrix: Matrix that indicates pair of
-            genes in the same cluster in csr_format.
+    - Solutions (ndarray): Matrix with all solutions readed from respective
+    function.
+        
+    Returns:
+    - connectivity Matrix: Matrix that indicates pair of genes in the same cluster 
+    in csr_format.
     """
     # Check not empty list.
     if len(Solution) == 0:
-        raise ValueError("La solución no puede estar vacía.")
+        raise ValueError("solution cannot be empty.")
         
     # Generate matches using NumPy for efficiency
     matches = np.equal.outer(Solution, Solution)
@@ -37,143 +38,121 @@ def compute_connectivity(Solution: list[int]) -> csr_matrix:
         
     return connectivity_matrix 
 
-def process_solution_batch(SolutionsBatch: list[list[int]]) -> list[csr_matrix]:
+
+def safe_compute_connectivity(Solution: np.ndarray):
     """
-    process_solution_batch(function):
-        Input:
-            - SolutionsBatch: Chunk of solutions that a process is
-            handling.
-        Output:
-            - Matrix that indicates pair of
-            genes in the same cluster in csr_format.
+    safe_compute_connectivity(function): Execute a exception handle 
+    version of compute_connectivity function.
+
+    Parameters:
+    - Solutions (ndarray): Matrix with all solutions readed from respective
+    function.
+
+    Returns:
+    - connectivity Matrix: Matrix that indicates pair of genes in the same cluster 
+    in csr_format.
     """
-    return [compute_connectivity(solution) for solution in SolutionsBatch]
+    try:
+        return compute_connectivity(Solution)
+    except Exception as e:
+        print(f"Unexpected error: {e}")
+        return None
 
 ######### Functions #########
 
-def connectivityMatrix_threads(SolutionsMatrix: list[list[int]], n_threads: int = 1) -> list[csr_matrix]:
-    """
-    connectivityMatrix_threads(function)
-        Input: 
-            - SolutionMatrix: Result from functions of reading input files
-            (allocates in 'ReadSolution.py' of this package).
-            - max_workers: Maximum of threads that are gonna process the
-            solutions 1 by 1.
-        Output:
-            - connectivityMatrix: List of sparse matrix that indicates
-            pair of gene who are in the same cluster in the respective
-            solution.
-        Description: Create a Matrix that create a square matrix with
-        max length dimesion from his solution (all equals). This says
-        what pair of index share the same number. Imagine the 
-        following example:
-           Solution 1 = [1, 2, 1, 2]
+"""
+This block contains all main functions.
+"""
 
-           Connectivity Matrix = [ [0,1,0,0] 
-                                   [1,0,0,1]
-                                   [0,0,1,0] ]
-        That is for every solution in solution matrix.
+def connectivityMatrix_threads(SolutionsMatrix: list[np.ndarray], n_threads: int = 1) -> list[csr_matrix]:
+    """
+    connectivityMatrix_threads(function): Create a Matrix that create a square matrix with
+    max length dimesion from his solution (all equals). This says what pair of index share 
+    the same number. Imagine the following example:
+    Solution 1 = [1, 2, 1, 2]
+
+    Connectivity Matrix = [ [0,1,0,0] 
+                            [1,0,0,1]
+                            [0,0,1,0] ]
+    That is for every solution in solution matrix.
+
+    Parameters: 
+    - SolutionMatrix (list[ndarray]): Result from functions of reading input files (allocates 
+    in 'ReadSolution.py' of this package).
+    - max_workers (int): Maximum of threads that are gonna process the solutions 1 by 1.
+        
+    Retruns:
+    - connectivityMatrix (list[csr_matrix]): List of sparse matrix that indicates pair of gene 
+    who are in the same cluster in the respective solution.
     """
     # Input Check.
     if isinstance(SolutionsMatrix, list):
         if not all(isinstance(row, list) for row in SolutionsMatrix):
-            raise ValueError("SolutionsMatrix debe ser una lista de listas.")
+            raise ValueError("SolutionsMatrix has to be a list of ndarray.")
     elif isinstance(SolutionsMatrix, np.ndarray):
         if SolutionsMatrix.ndim != 2:
-            raise ValueError("SolutionsMatrix debe ser un arreglo NumPy 2D.")
+            raise ValueError("SolutionsMatrix has to be in 2D.")
     if n_threads <= 0:
         raise ValueError("n_threads must be a positive integer.")
     
+    # Exception handling.
     try:
-        
         # Using 'ThreadPoolExecutor' to make a parallel processing of
         # solutions.
         with ThreadPoolExecutor(max_workers=n_threads) as executor:
-            connectivityMatrix = list(executor.map(compute_connectivity, SolutionsMatrix))
+            connectivityMatrix = list(executor.map(safe_compute_connectivity, SolutionsMatrix))
         
         # Output.
         return connectivityMatrix
     
-    # Exceptions block.
+    # Exceptions message block.
     except Exception as e:
-        print(f"Ocurrió un error inesperado: {e}")
-
-def connectivityMatrix_processes(SolutionsMatrix: list[list[int]], n_jobs: int = os.cpu_count()) -> list[csr_matrix]:
-    """
-        connectivityMatrix_processes(function)
-        Input: 
-            - SolutionMatrix: Result from functions of reading input files
-            (allocates in 'ReadSolution.py' of this package).
-            - max_workers: Maximum of threads that are gonna process the
-            solutions 1 by 1.
-        Output:
-            - connectivityMatrix: List of sparse matrix that indicates
-            pair of gene who are in the same cluster in the respective
-            solution.
-        Description: Create a Matrix that create a square matrix with
-        max length dimesion from his solution (all equals). This says
-        what pair of index share the same number. Imagine the 
-        following example:
-           Solution 1 = [1, 2, 1, 2]
-
-           Connectivity Matrix = [ [0,1,0,0] 
-                                   [1,0,0,1]
-                                   [0,0,1,0] ]
-        That is for every solution in solution matrix.
-    """
-    # Input Check
-    if not isinstance(SolutionsMatrix, list) or not all(isinstance(x, list) for x in SolutionsMatrix):
-        raise ValueError("SolutionsMatrix must be a list of lists.")
-    if n_jobs <= 0:
-        raise ValueError("n_jobs must be a positive integer.")
-    
-    try:
-        # Divide solutions considering cores.
-        chunk_size = max(1, len(SolutionsMatrix) // n_jobs)
-        solution_chunks = [SolutionsMatrix[i:i + chunk_size] for i in range(0, len(SolutionsMatrix), chunk_size)]
-
-        # Use ProcessPoolExecutor to process each chunk.
-        with ProcessPoolExecutor(max_workers=n_jobs) as executor:
-            results = executor.map(process_solution_batch, solution_chunks)
-        
-        # unit all results.
-        connectivityMatrix = list(itertools.chain.from_iterable(results))
-        
-        return connectivityMatrix
-
-    except Exception as e:
-        print(f"Ocurrió un error inesperado: {e}")
-        return None
+        print(f"Unexpected error: {e}")
 
 def sum_connectivity_matrices(connectivity_matrix: list[csr_matrix]) -> csr_matrix:
     """
-    sum_connectivity_matrices(funcion)
-        Input:
-            - connectivity_matrix: List of sparce matrix from
-            'connectivityMatrix' function (csr format).
-        Output:
-            - summed_matrix: Sum of all connectivity matrix
-            from input (csr format).
-        Description: Sum all connectivity matrix in the input 
-        ('connectivity_matrix' for more information).
-    """    
-    # Check if the list is empty
-    if len(connectivity_matrix) == 0:
-            raise ValueError("La lista de matrices de conectividad está vacía.")
-
-    # Ensure all elements are CSR matrices
-    if not all(isinstance(matrix, csr_matrix) for matrix in connectivity_matrix):
-        raise TypeError("Todos los elementos deben ser matrices dispersas CSR.")
+    sum_connectivity_matrices(funcion): Sum all connectivity matrix in the input 
+    ('connectivityMatrix_threads' for more information).
+    
+    Parameters:
+    - connectivity_matrix (list[csr_matrix]): List of sparce matrix from
+    'connectivityMatrix_threads' function (csr format).
+    
+    Returns:
+    - summed_matrix (csr_matrix): Sum of all connectivity matrix from input (csr format).
+    """ 
+    # Input check.
+    if not connectivity_matrix:
+        raise ValueError("empty list.")
     
     first_shape = connectivity_matrix[0].shape
     if not all(matrix.shape == first_shape for matrix in connectivity_matrix):
-        raise ValueError("Todas las matrices deben tener las mismas dimensiones.")
+        raise ValueError("All matrix needs to have the same shape.")
     
+    # Exception handling.
     try:
         # Sum connectivity matrix.
-        summed_matrix = sum(connectivity_matrix[1:], start=connectivity_matrix[0])
-        return summed_matrix
+        return sum(connectivity_matrix[1:], start=connectivity_matrix[0])
     
+    # Exceptions message block.
     except Exception as e:
-        print(f"Ocurrió un error inesperado: {e}")
-        return None
+        raise RuntimeError(f"Unexpected error: {e}")
+    
+def save_connectivity_matrix_as_csv(matrix: csr_matrix, filepath: str) -> None:
+    """
+    save_connectivity_matrix_as_csv(function): Save CSR matrix as csv file.
+    
+    Parameters:
+    - matrix (csr_matrix): CSR matrix to save.
+    - filepath (str): Path where the file is going to be allocated.
+    """
+    # Input Check.
+    if not isinstance(matrix, csr_matrix):
+        raise ValueError("Matrix has to be in csr format.")
+    
+    try:
+        dense_matrix = matrix.toarray()
+        np.savetxt(filepath, dense_matrix, delimiter=",", fmt="%d")
+        print(f"Matrix save successful {filepath}")
+    except Exception as e:
+        print(f"Unexpected error: {e}")
